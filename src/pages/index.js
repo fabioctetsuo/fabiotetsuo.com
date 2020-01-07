cfbb5291af5ddd6b28f0a6a1e7f6534dab3a0f06
Fabio Tetsuo <fabioctetsuo@gmail.com>
2020-01-07 01:36:34 -0300

    fix: adjusting export page component
    fixing build error in netlify for export page component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,50 @@
 import React from 'react';
-import AboutPage from './about';
+import styled from 'styled-components';
+import media from 'styled-media-query';
 
-const IndexPage = () => <AboutPage />;
+import Layout from '../components/Layout';
+import Introduction from '../modules/About/Introduction';
+import Resume from '../modules/About/Resume';
+import SEO from '../components/seo';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 0 20%;
+  background-color: ${({ color }) => color || 'none'};
+
+  ${media.lessThan('large')`
+    padding: 0 24px;
+  `}
+
+  ${media.lessThan('medium')`
+    padding: 0 16px;
+  `}
+`;
+
+const Row = styled.div`
+  width: 100%;
+  height: auto;
+  padding: ${({ padding }) => padding || '0'};
+`;
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Container>
+        <Row>
+          <Introduction />
+        </Row>
+      </Container>
+      <Container color="#6A5ACD">
+        <Row padding="48px 0 48px 0">
+          <Resume />
+        </Row>
+      </Container>
+    </Layout>
+  );
+}
 
 export default IndexPage;
 
