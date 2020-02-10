ca50b923df7f98f441d1ac7f045502b62849481c
Fabio Tetsuo <fabioctetsuo@gmail.com>
2020-02-10 01:12:41 -0300

    refactor: adjusting background color

--- src/components/Sidebar/styled.jsx
+++ src/components/Sidebar/styled.jsx
@@ -4,7 +4,7 @@ import media from 'styled-media-query'
 export const SidebarWrapper = styled.aside`
   align-items: center;
   border-right: 1px solid var(--borders);
-  background: var(--mediumBackground);
+  background: #FFF;
   display: flex;
   flex-direction: column;
   height: 100vh;
