49cd2939ec1c8382c50d4e66e901ae310cb965b3
Fabio Tetsuo <fabioctetsuo@gmail.com>
2020-02-10 14:17:15 -0300

    feat: adding aria-label to improve accessibility

--- src/components/SocialLinks/index.jsx
+++ src/components/SocialLinks/index.jsx
@@ -8,6 +8,7 @@ const SocialLinks = () => (
       const Icon = S.Icons[link.label];
       return (
         <a
+          aria-label={link.label}
           href={link.url}
           title={link.label}
           target="_blank"
