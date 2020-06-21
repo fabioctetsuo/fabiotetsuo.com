97471a229acf3842ed41c248a2f64be4fe066330
Fabio Tetsuo <fabio.tetsuo@gympass.com>
2020-06-21 10:52:49 -0300

    feat(Skills): adding Skill containers

--- src/modules/About/Resume/index.jsx
+++ src/modules/About/Resume/index.jsx
@@ -1,8 +1,143 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 import * as Typography from '../../../components/Typography';
 import * as S from './styles';
 
 const Resume = () => {
+  const {
+    babelLogo,
+    reactLogo,
+    gatsbyLogo,
+    emberLogo,
+    nodeLogo,
+    javascriptLogo,
+    htmlLogo,
+    cssLogo,
+    jestLogo,
+    webpackLogo,
+    gitLogo,
+    jenkinsLogo,
+    npmLogo,
+    netlifyLogo,
+  } = useStaticQuery(graphql`
+    query Skills {
+      reactLogo: file(relativePath: {eq: "skills/react.png"}) {
+        childImageSharp {
+          fluid (maxWidth: 52) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      gatsbyLogo: file(relativePath: {eq: "skills/gatsby.png"}) {
+        childImageSharp {
+          fluid (maxWidth: 52) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      babelLogo: file(relativePath: {eq: "skills/babeljs.png"}) {
+        childImageSharp {
+          fluid (maxWidth: 52) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      emberLogo: file(relativePath: {eq: "skills/emberjs.png"}) {
+        childImageSharp {
+          fluid (maxWidth: 52) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      nodeLogo: file(relativePath: {eq: "skills/nodejs.png"}) {
+        childImageSharp {
+          fluid (maxWidth: 52) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      javascriptLogo: file(relativePath: {eq: "skills/javascript.png"}) {
+        childImageSharp {
+          fluid (maxWidth: 52) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      htmlLogo: file(relativePath: {eq: "skills/html5.png"}) {
+        childImageSharp {
+          fluid (maxWidth: 52) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      cssLogo: file(relativePath: {eq: "skills/css3.png"}) {
+        childImageSharp {
+          fluid (maxWidth: 52) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      jestLogo: file(relativePath: {eq: "skills/jest.png"}) {
+        childImageSharp {
+          fluid (maxWidth: 52) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      webpackLogo: file(relativePath: {eq: "skills/webpack.png"}) {
+        childImageSharp {
+          fluid (maxWidth: 52) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      gitLogo: file(relativePath: {eq: "skills/git.png"}) {
+        childImageSharp {
+          fluid (maxWidth: 52) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      jenkinsLogo: file(relativePath: {eq: "skills/jenkins.png"}) {
+        childImageSharp {
+          fluid (maxWidth: 52) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      npmLogo: file(relativePath: {eq: "skills/npm.png"}) {
+        childImageSharp {
+          fluid (maxWidth: 52) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      netlifyLogo: file(relativePath: {eq: "skills/netlify.png"}) {
+        childImageSharp {
+          fluid (maxWidth: 52) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+  const skills = [
+    { name: 'React JS', image: reactLogo },
+    { name: 'Gatsby', image: gatsbyLogo },
+    { name: 'Ember JS', image: emberLogo },
+    { name: 'Node JS', image: nodeLogo },
+    { name: 'Javascript', image: javascriptLogo },
+    { name: 'HTML 5', image: htmlLogo },
+    { name: 'CSS 3', image: cssLogo },
+    { name: 'Jest', image: jestLogo },
+    { name: 'Webpack', image: webpackLogo },
+    { name: 'Babel JS', image: babelLogo },
+    { name: 'Git', image: gitLogo },
+    { name: 'Jenkins', image: jenkinsLogo },
+    { name: 'NPM', image: npmLogo },
+    { name: 'Netlify', image: netlifyLogo },
+  ];
+
   return (
     <S.Container>
       <S.AboutWrapper>
@@ -26,6 +161,15 @@ const Resume = () => {
             Skills
           </Typography.Heading>
         </S.TitleWrapper>
+        <S.SkillsWrapper>
+          {skills.map(skill => (
+            <S.SkillItem
+              fluid={skill.image.childImageSharp.fluid}
+              alt={skill.name}
+              key={skill.name}
+            />
+          ))}
+        </S.SkillsWrapper>
       </S.SkillsContainer>
     </S.Container>
   )
