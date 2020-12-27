c0935a76dfd5eaa8fb00a6d85aea054d262078cf
Fabio Tetsuo <fabioctetsuo@gmail.com>
2020-12-27 14:25:38 -0300

    fix: add material ui plugin for gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-material-ui`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
